fbe4c5cdbb4d05a9d2ad3fdd7b10dd4f9f8a7ed0
Geoff.Lewis <Geoff.Lewis@atmosi.com>
2020-07-21 10:45:52 +0100

    added MVC .net core web api to serve the batch bar image. Renders a dynamic bitmap of some random batches. Added a bathes and batches by date report that queries for a specified number of batches and also a date

---
+++ SSRSMVCDataService/Controllers/BatchesController.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace SSRSMVCDataService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BatchesController : ControllerBase
+    {
+        private IWebHostEnvironment m_WebHostEnvironment;
+
+        private Color[] BatchColours => new Color[] 
+        {
+            Color.Aquamarine,
+            Color.IndianRed,
+            Color.OrangeRed,
+            Color.MediumPurple,
+            Color.DeepPink,
+            Color.LightSeaGreen
+        };
+
+        public BatchesController(IWebHostEnvironment webHostEnvironment)
+        {
+            m_WebHostEnvironment = webHostEnvironment;
+        }
+
+        public ActionResult Image(string numberOfBatches = "4")
+        
+        {
+            //using (var reader = new StreamReader(Request.Body))
+            //{
+            //    var body = reader.ReadToEnd();
+
+            //    // Do something
+            //}
+
+            //var dir = $"{m_WebHostEnvironment.ContentRootPath }\\Images";
+            //var path = Path.Combine(dir, "SampleBatchBar.png"); //validate the path for security or use other means to generate the path.
+
+            //return PhysicalFile(path, "image/png");
+
+            var batchesImage = GetBatchesBitmap(Convert.ToInt32(numberOfBatches));
+            return File(ImageToByteArray(batchesImage), "image/png");
+        }
+
+        private Bitmap GetBatchesBitmap(int numberOfBatches)
+        {
+            var totalLength = 100;
+            var ran = new Random();
+            var allBatches = Enumerable.Range(1, numberOfBatches - 1).Select(index =>
+             {
+                 return new Batch(ran.Next(10, 20),
+                      BatchColours[ran.Next(0, BatchColours.Length - 1)]);
+             }).ToList();
+            allBatches.Add(new Batch(totalLength - allBatches.Sum(b => b.Length), BatchColours[ran.Next(0, BatchColours.Length - 1)]));
+
+            var image = new Bitmap(500, 100);
+            using(var g = Graphics.FromImage(image))
+            {
+                foreach(var batch in allBatches)
+                {
+                    g.FillRectangle(new SolidBrush(batch.Colour),allBatches.TakeWhile(b => !(b == batch)).Sum(previousBatch => previousBatch.Length * 5),0,batch.Length * 5,100);
+                }
+            }
+
+            return image;
+        }
+        private byte[] ImageToByteArray(System.Drawing.Image imageIn)
+        {
+            MemoryStream ms = new MemoryStream();
+            imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            return ms.ToArray();
+        }
+
+        private class Batch
+        {
+            public int Length { get; }
+            public Color Colour { get; }
+
+            public Batch(int length, Color colour)
+            {
+                Length = length;
+                Colour = colour;
+            }
+        }
+
+    }
+}
---
+++ SSRSWebDataProvider/Controllers/BatchController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SSRSWebDataProvider.Controllers
+{
+    public class BatchController : ApiController
+    {
+        public ActionResult BatchBar()
+        {
+            throw new NotImplementedException();
+            //code not working taken from a suggested answer
+
+            //var dir = Server.MapPath("/Images");
+            //var path = Path.Combine(dir, "SampleBatchBar.png");
+            //return base.File(path, "image/jpeg");
+        }
+    }
+}
